253c101244dae1c3108cb54778be595cffadbc8f
Herni <hernanatn@gmail.com>
2025-02-15 16:36:03 -0300

    Agrega StartingPosition para CIVILIZATION_WHITEWALKER en GSWesterosStandard y GSWesterosEssosHuge

---
+++ AddOns/ACoIaF - Gathering Storm/Scripts/ACoIaF_WW_TSL.sql
@@ -0,0 +1,4 @@
+INSERT INTO StartPosition (MapName, CivilizationType, X, Y)
+VALUES
+    ('GSWesterosStandard', 'CIVILIZATION_WHITEWALKER', 3, 77),
+    ('GSWesterosEssosHuge', 'CIVILIZATION_WHITEWALKER', 3, 77);
